5eaa10cc56379ebbeba2bc207f5aac0e460254fa
asyahra19 <asyahrara02@student.uns.ac.id>
2023-06-06 01:23:26 +0700

    make:model, not filled yet

---
+++ app/Models/Administrator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Administrator extends Model
+{
+    protected $table = 'administrator';
+    protected $primaryKey = 'id_admin';
+}
---
+++ app/Models/Jenis_Pengguna.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jenis_Pengguna extends Model
+{
+    protected $table = 'jenis_pengguna';
+    protected $primaryKey = 'id_jenis_pengguna';
+}
